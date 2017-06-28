864485603efa161497eca93ebf166c978d7fdb6e
Viktor Kirilenko <vkirilenko@bellintegrator.ru>
2017-06-28 10:29:30 +0300

    qw

---
+++ js/mainscript.js
@@ -0,0 +1,28 @@
+function initiate() {
+    var bt = document.formdata.btAdd;
+    var input = document.formdata.input;
+    var result = document.getElementById("div-result");
+
+    if (bt == null || input == null || result == null) {
+        throw "Не найден один из элементов для корректной работы."
+    }
+
+    bt.onclick = function () {
+        var val = input.value.trim();
+        if (val != "") {
+            addText(val);
+            input.value = null;
+        }
+        else {
+            console.log("пустой текст2");
+        }
+    }
+
+    function addText(text) {
+        var elem = document.createElement("p");
+        elem.textContent = text;
+        result.appendChild(elem);
+    }
+}
+
+window.addEventListener("load", initiate, false);
